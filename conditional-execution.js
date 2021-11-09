f64b73b6d2cda97307ae717165a357de70475c42
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2021-11-09 00:38:46 +0000

    Add even more conditional examples

--- conditional-execution.js
+++ conditional-execution.js
@@ -1,3 +1,24 @@
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
 const temperature = 30;
 
 if (temperature < 0) {
